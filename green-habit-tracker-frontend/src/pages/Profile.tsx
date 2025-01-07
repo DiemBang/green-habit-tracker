e29287527582752db9f37dd9568bbc350e20ab50
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-07 10:26:22 +0100

    experiment with avatar

--- green-habit-tracker-frontend/src/pages/Profile.tsx
+++ green-habit-tracker-frontend/src/pages/Profile.tsx
@@ -1,7 +1,16 @@
+import avatar from "../assets/avatar.svg";
+
 export const Profile = () => {
   return (
     <>
       <p>This is Profile</p>
+      <section>
+        <img
+          src={avatar}
+          alt="avatar profile"
+          className="w-32 h-32 rounded-full border-4 border-green-500"
+        />
+      </section>
     </>
   );
 };
