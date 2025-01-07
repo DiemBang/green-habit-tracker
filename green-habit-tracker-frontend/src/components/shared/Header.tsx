f3b9d600725e1f8b15d8379c9e5d299d5d6d5e86
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-07 20:52:33 +0100

    WIP add back button in header

--- green-habit-tracker-frontend/src/components/shared/Header.tsx
+++ green-habit-tracker-frontend/src/components/shared/Header.tsx
@@ -1,6 +1,10 @@
+import { useLocation } from "react-router-dom";
 import logotype from "../../assets/category-icons/all-category.png";
 
 export const Header = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
   return (
     <>
       <header className="w-screen h-16 bg-green-100 fixed top-0 rounded-b-lg flex items-center justify-center">
