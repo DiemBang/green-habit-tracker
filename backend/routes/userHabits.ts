64e390bd05f273de30e7e8dc4a4a6f82cb71ae25
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-09 22:17:38 +0100

    add post endpoint for userHabit

--- backend/routes/userHabits.ts
+++ backend/routes/userHabits.ts
@@ -17,6 +17,37 @@ router.get("/", function (req: Request, res: Response) {
     });
 });
 
+/* Add habit for User */
+router.post("/add", async (req: Request, res: Response) => {
+  console.log("Incoming request body:", req.body);
+  try {
+    const dateStarted: Date = new Date();
+
+    // Create New User Object
+    const userHabit = {
+      UserID: req.body.userID,
+      HabitIdentifier: req.body.habitIdentifier,
+      DateStarted: dateStarted,
+      ReminderTime: req.body.reminderTime,
+      Frequency: req.body.frequency,
+    };
+
+    // Insert userHabit into Database
+    const result = await req.app.locals.db
+      .collection("UserHabit")
+      .insertOne(userHabit);
+    console.log("Insert Result:", result);
+
+    res.json({
+      message: `New userHabit added with ID ${result.insertedId}`,
+      userHabitID: result.insertedId,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // ToDo: GET userhabit for specific user
 
 export default router;
