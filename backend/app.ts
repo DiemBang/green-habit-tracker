d15dbd84e5bdc2854232830a8dd5ad735e0f1e6d
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-09 12:15:30 +0100

    add endpoint for get specific user + error handling

--- backend/app.ts
+++ backend/app.ts
@@ -17,6 +17,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
 app.use(cors());
 
 app.use("/api/habits", habitRouter);
--- backend/routes/users.ts
+++ backend/routes/users.ts
@@ -3,9 +3,10 @@ import { IUser } from "../models/IUser";
 
 var express = require("express");
 const router = Router();
+const ObjectId = require("mongodb").ObjectId;
 
-/* GET habitTypes */
-router.get("/", function (req: Request, res: Response) {
+/* GET all users */
+router.get("/", (req: Request, res: Response) => {
   // HÄMTA (anropa databas connection som skapades i app.js )
   req.app.locals.db
     .collection("User")
@@ -14,6 +15,35 @@ router.get("/", function (req: Request, res: Response) {
     .then((results: Array<IUser>) => {
       console.log("results", results);
       res.json(results);
+    });
+});
+
+/* GET specific users */
+router.post("/", async (req: Request, res: Response): Promise<any> => {
+  let findUser;
+  try {
+    findUser = new ObjectId(req.body._id);
+  } catch (error) {
+    return (res as Response).json({ error: "Invalid User ID format." });
+  }
+  req.app.locals.db
+    .collection("User") // Specify the type for the collection
+    .find({ _id: findUser })
+    .toArray()
+    .then((results: IUser[]) => {
+      if (results.length === 0) {
+        // Handle case where no user is found
+        return res.status(404).json({ error: "User not found." });
+      }
+      console.log("results", results);
+      res.json(results);
+    })
+    .catch((dbError: unknown) => {
+      // Handle database errors
+      console.error("Database error:", dbError);
+      res
+        .status(500)
+        .json({ error: "Internal Server Error. Please try again later." });
     });
 });
 
