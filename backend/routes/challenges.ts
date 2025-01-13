55ed8daa066ab741b90b1bd986de3a68d1e2f4fb
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-13 11:51:06 +0100

    add endpoint for get challenge for specific month

--- backend/routes/challenges.ts
+++ backend/routes/challenges.ts
@@ -4,7 +4,7 @@ import { IChallenge } from "../models/IChallenge";
 var express = require("express");
 const router = Router();
 
-/* GET ecotips */
+/* GET challenges */
 router.get("/", function (req: Request, res: Response) {
   // HÄMTA (anropa databas connection som skapades i app.js )
   req.app.locals.db
@@ -12,6 +12,24 @@ router.get("/", function (req: Request, res: Response) {
     .find()
     .toArray()
     .then((results: Array<IChallenge>) => {
+      console.log("results", results);
+      res.json(results);
+    });
+});
+
+/* GET challenges for specific month */
+router.get("/current", async (req: Request, res: Response): Promise<any> => {
+  const currentMonth = new Date().getMonth() + 1;
+
+  req.app.locals.db
+    .collection("Challenge") // Specify the type for the collection
+    .find({ monthAvailableToJoin: currentMonth })
+    .toArray()
+    .then((results: IChallenge[]) => {
+      if (results.length === 0) {
+        // Handle case where no challenge is found
+        return res.status(404).json({ error: "Challenge not found." });
+      }
       console.log("results", results);
       res.json(results);
     });
