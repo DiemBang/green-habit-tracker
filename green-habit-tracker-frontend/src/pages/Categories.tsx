c5496ea124c81af35e97b3637b032caf3a54aea1
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-16 22:50:41 +0100

    add styling to categories page

--- green-habit-tracker-frontend/src/pages/Categories.tsx
+++ green-habit-tracker-frontend/src/pages/Categories.tsx
@@ -5,38 +5,41 @@ import foodCategoryIcon from "../assets/category-icons/food-category.png";
 import onthegoCategoryIcon from "../assets/category-icons/onthego-category.png";
 import workCategoryIcon from "../assets/category-icons/work-category.png";
 import allCategoryIcon from "../assets/category-icons/all-category.png";
+import { CardSection } from "../components/CardSection";
 
 export const Categories = () => {
   return (
     <>
       <h2>Categories</h2>
-      <section className="grid grid-cols-2 gap-4">
-        <Link to="/habitsPerCategory/Home">
-          <CategoryButton
-            category={{ iconUrl: homeCategoryIcon, name: "Home" }}
-          ></CategoryButton>
-        </Link>
-        <Link to="/habitsPerCategory/Food">
-          <CategoryButton
-            category={{ iconUrl: foodCategoryIcon, name: "Food" }}
-          ></CategoryButton>
-        </Link>
-        <Link to="/habitsPerCategory/On-the-go">
-          <CategoryButton
-            category={{ iconUrl: onthegoCategoryIcon, name: "On-the-go" }}
-          ></CategoryButton>
-        </Link>
-        <Link to="/habitsPerCategory/Work">
-          <CategoryButton
-            category={{ iconUrl: workCategoryIcon, name: "Workplace" }}
-          ></CategoryButton>
-        </Link>
-        <Link to="/habitsPerCategory/All">
-          <CategoryButton
-            category={{ iconUrl: allCategoryIcon, name: "All" }}
-          ></CategoryButton>
-        </Link>
-      </section>
+      <CardSection>
+        <section className="grid grid-cols-2 gap-4">
+          <Link to="/habitsPerCategory/Home">
+            <CategoryButton
+              category={{ iconUrl: homeCategoryIcon, name: "Home" }}
+            ></CategoryButton>
+          </Link>
+          <Link to="/habitsPerCategory/Food">
+            <CategoryButton
+              category={{ iconUrl: foodCategoryIcon, name: "Food" }}
+            ></CategoryButton>
+          </Link>
+          <Link to="/habitsPerCategory/On-the-go">
+            <CategoryButton
+              category={{ iconUrl: onthegoCategoryIcon, name: "On-the-go" }}
+            ></CategoryButton>
+          </Link>
+          <Link to="/habitsPerCategory/Work">
+            <CategoryButton
+              category={{ iconUrl: workCategoryIcon, name: "Workplace" }}
+            ></CategoryButton>
+          </Link>
+          <Link to="/habitsPerCategory/All">
+            <CategoryButton
+              category={{ iconUrl: allCategoryIcon, name: "All" }}
+            ></CategoryButton>
+          </Link>
+        </section>
+      </CardSection>
     </>
   );
 };
