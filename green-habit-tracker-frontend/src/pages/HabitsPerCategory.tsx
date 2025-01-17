8ef4c12d8f2383fcfad2302bd65de9f814edf146
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-18 00:26:02 +0100

    update heading to use useparams

--- green-habit-tracker-frontend/src/pages/HabitsPerCategory.tsx
+++ green-habit-tracker-frontend/src/pages/HabitsPerCategory.tsx
@@ -1,17 +1,14 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import { IHabit } from "../models/IHabit";
 import { HabitButton } from "../components/HabitButton";
 
 export const HabitsPerCategory = () => {
-  // TODO:
-  // 3. Get category from URL paremeter
-  // 4. In backend, allow filtering habit type by category
-  // 5. Use category from URL parameter to filter habit types
   const habitTypes = useLoaderData() as IHabit[];
+  let { category } = useParams();
 
   return (
     <>
-      <h2>Habits per Category</h2>
+      <h2>{category}</h2>
       {habitTypes.map((habitType) => (
         <HabitButton key={habitType._id} habitType={habitType}></HabitButton>
       ))}
