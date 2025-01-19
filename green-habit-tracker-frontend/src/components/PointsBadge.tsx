e89e0198487c6f08452273d17a9e876a31b51b7b
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-19 18:47:14 +0100

    fix points icon on habit page

--- green-habit-tracker-frontend/src/components/PointsBadge.tsx
+++ green-habit-tracker-frontend/src/components/PointsBadge.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-
+import coinIcon from "/src/assets/coin.svg";
 interface PointsBadgeProps {
   children: ReactNode; // Accepts any valid React children
 }
@@ -8,11 +8,7 @@ export const PointsBadge: React.FC<PointsBadgeProps> = ({ children }) => {
   return (
     <>
       <div className="flex items-center bg-yellow-100 text-yellow-800 font-semibold px-2 py-1 rounded-md shadow-sm max-w-fit">
-        <img
-          src="../src/assets/coin.svg"
-          alt="Points icon"
-          className="w-5 h-5 mr-1"
-        />
+        <img src={coinIcon} alt="Points icon" className="w-5 h-5 mr-1" />
         {children}
       </div>
     </>
