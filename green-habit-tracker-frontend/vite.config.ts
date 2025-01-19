2696a822629008c63f7f7fc5119597fe2059dee7
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-19 18:17:37 +0100

    Update vite.config.ts

--- green-habit-tracker-frontend/vite.config.ts
+++ green-habit-tracker-frontend/vite.config.ts
@@ -4,8 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src", // Alias for the `src` folder
+    },
+  },
+  base: "./",
   build: {
     outDir: "dist",
     emptyOutDir: true, // also necessary
+    rollupOptions: {
+      input: "index.html", // Ensure it starts from your index.html
+    },
   },
 });
