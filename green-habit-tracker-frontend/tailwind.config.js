c6a06e1ca68d0b315493a7ab79945cbba7d41fb9
Diem Bang <72154226+DiemBang@users.noreply.github.com>
2025-01-29 22:16:47 +0100

    remove unused custom colors

--- green-habit-tracker-frontend/tailwind.config.js
+++ green-habit-tracker-frontend/tailwind.config.js
@@ -6,12 +6,8 @@ export default {
       colors: {
         cloudWhite: "#FFFAF2", // Backgrounds
         calmBlue: "#BCCEDA", // Secondary Buttons
-        mutedTeal: "#9FB8AD", // Highlights/Badges
-        sandstone: "#E8D8C3", // Secondary Backgrounds
         charcoalGray: "#495057", // Text, Header, Footer
         fontPrimary: "#495057", // Charcoal Gray
-        fontAccent: "#9FB8AD", // Muted Teal
-        fontMuted: "#BCCEDA", // Calm Blue
         buttonGreen: "#acd980",
         customGreen: "#107f39",
         customBlue: "#4784aa",
